0588260a9f6937e8c74466ffd7739ae3fa84b363
ZMr-P <technologymr.p@gmail.com>
2024-03-11 16:37:43 -0300

    FirstCommit

---
+++ TesteEstagio/TesteEstagio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TesteEstagio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sequencia = double.Parse(Console.ReadLine());
+
+            if (EFibonacci(sequencia))
+            {
+                Console.WriteLine($"{sequencia} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"{sequencia} não pertence à sequência de Fibonacci.");
+            }
+        }
+        public static bool QuaPerfeito(double n)
+        {
+            double raiz;
+            raiz = Math.Sqrt(n);
+            return raiz * raiz == n;
+        }
+        public static bool EFibonacci(double n)
+        {
+            return QuaPerfeito(5 * n * n - 4) || QuaPerfeito(5 * n * n + 4);
+        }
+    }
+}
---
+++ TesteEstagio2/TesteEstagio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TesteEstagio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra = Console.ReadLine();
+
+            Console.WriteLine(new string(new Stack<char>(palavra).ToArray()));          
+        }
+    }
+}
